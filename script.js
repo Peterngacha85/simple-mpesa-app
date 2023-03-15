48d4d1ae312a6e84d09eb03bcc30fe1098223b56
Peter Ngacha <96945268+Peterngacha85@users.noreply.github.com>
2023-03-16 00:12:15 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+const allServicesBtn = document.getElementsByClassName('services_hide');
+const single_service = document.getElementsByClassName('single_service');
+const HOME = document.getElementsByClassName('HOME');
+
+function home(){
+    allServicesBtn[0].style.zIndex = 1;
+    single_service[0].style.zIndex = 0;
+    single_service[1].style.zIndex = 0;
+    single_service[3].style.zIndex = 0;
+    single_service[4].style.zIndex = 0;
+    single_service[5].style.zIndex = 0;
+    single_service[6].style.zIndex = 0;
+
+
+
+}
+function sending(){
+    single_service[0].style.zIndex = 1;
+    allServicesBtn[0].style.zIndex = 0;
+}
+function withdraw (){
+    single_service[1].style.zIndex = 1;
+    single_service[0].style.zIndex = 0;
+}
+function buy_airtime(){
+    single_service[2].style.zIndex = 1;
+    allServicesBtn[0].style.zIndex = 0;
+}
+function loan_saving(){
+    single_service[3].style.zIndex = 1;
+    allServicesBtn[0].style.zIndex = 0
+}
+function lipa_na_mpesa(){
+    single_service[4].style.zIndex = 1;
+    allServicesBtn[0].style.zIndex = 0;
+
+}
+function my_account(){
+    single_service[5].style.zIndex = 1;
+    allServicesBtn[0].style.zIndex = 0;
+}
